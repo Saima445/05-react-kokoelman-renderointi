36456e94d00fe4b5fe8c0e7d401193992976b0b5
SaimaS <saima.salonen@gmail.com>
2024-02-27 16:29:10 +0200

    02 poistettu turhia tiedostoja ja muokattu app.jsx tehtävänannon alkutilanteeseen. Buttoniin lisätty tapahtumankäsittelijä nuolisyntaksilla funktioon.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App(props) {
+  const [value, setValue] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {value}
+      <button onClick={() => setValue(0)}>button</button>
     </>
-  )
+  );
 }
 
-export default App
+// HUOM Hookeja siis kuuluu kutsua ainoastaan React-komponentin
+// määrittelevän funktion rungosta:
+
+// const App = (props) => {
+//   // nämä ovat ok
+//   const [age, setAge] = useState(0)
+//   const [name, setName] = useState('Juha Tauriainen')
+
+//   if ( age > 10 ) {
+//     // ei ehtolauseessa
+//     const [foobar, setFoobar] = useState(null)
+//   }
+
+//   for ( let i = 0; i < age; i++ ) {
+//     // eikä myöskään loopissa
+//     const [rightWay, setRightWay] = useState(false)
+//   }
+
+//   const notGood = () => {
+//     // ei muiden kuin komponentin määrittelevän funktion sisällä
+//     const [x, setX] = useState(-1000)
+//   }
+
+//   return (
+//     //...
+//   )
+// }
+
+export default App;
