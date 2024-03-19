57dadc420cb2fc0a0154119e42876c44bbb03be9
SaimaS <saima.salonen@gmail.com>
2024-03-19 12:06:19 +0200

    tehtävän lähtötilanne

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { useState } from "react";
 
-function App(props) {
-  return <></>;
-}
+const App = (props) => {
+  const { notes } = props;
+  return (
+    <>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+    </>
+  );
+};
 
 export default App;
