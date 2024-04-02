b495da1528aa421f251133374a5e540d6a0d03d1
SaimaS <saima.salonen@gmail.com>
2024-04-02 12:32:50 +0300

    palvelimen kanssa tapahtuva kommunikointi eriytetty omaan moduuliin

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
+  useEffect(() => {
+    noteService.getAll().then((response) => {
       setNotes(response.data);
     });
-  };
-  useEffect(hook, []);
+  }, []);
 
   //toinen tapa EVENTHANDLERIN KANSSA
   // useEffect(() => {
@@ -40,12 +38,10 @@ const App = () => {
   // console.log("render", notes.length, "notes");
 
   const toggleImportanceOf = (id) => {
-    console.log(`importance of ${id} needs to be toggled`);
-    const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
 
-    axios.put(url, changedNote).then((response) => {
+    noteService.update(id, changedNote).then((response) => {
       setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
     });
   };
@@ -55,12 +51,10 @@ const App = () => {
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
-      // id: notes.length + 1,
     };
 
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-      console.log(response);
-      setNotes(notes.concat(noteObject));
+    noteService.create(noteObject).then((response) => {
+      setNotes(notes.concat(response.data));
       setNewNote("");
     });
   };
@@ -89,22 +83,19 @@ const App = () => {
       {/* tapa 2 käyttäen map-metodia */}
       {/* <h1>Notes (map)</h1>
       <ul>
-        {notes.length > 0 &&
-          notes.map((note) => <li key={note.id}>{note.content}</li>)}
+        {notes.map((note) => <li key={note.id}>{note.content}</li>)}
       </ul>
       <hr></hr> */}
       {/* tapa 3 antipattern, ei ole suositeltava tapa! */}
       {/* <h1>Notes (taulukon indeksit avaimina)</h1>
       <ul>
-        {notes.length > 0 &&
-          notes.map((note, i) => <li key={i}>{note.content}</li>)}
+        {notes.map((note, i) => <li key={i}>{note.content}</li>)}
       </ul>
       <hr></hr> */}
       {/* tapa 4 tehdään Notelle oma komponentti */}
       {/* <h1>Notes (omalla komponentilla)</h1>
       <ul>
-        {notes.length > 0 &&
-          notes.map((note) => <Note key={note.id} note={note} />)}
+        {notes.map((note) => <Note key={note.id} note={note} />)}
       </ul>
       <hr></hr> */}
       {/* tapa 5 tallettaa muuttujaan notesToShow ehdoilla (muuttujalla notesToShow/important:all) */}
@@ -115,14 +106,13 @@ const App = () => {
         </button>
       </div>
       <ul>
-        {notes.length > 0 &&
-          notesToShow.map((note) => (
-            <Note
-              key={note.id}
-              note={note}
-              toggleImportance={() => toggleImportanceOf(note.id)}
-            />
-          ))}
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        ))}
       </ul>
       <form onSubmit={addNote}>
         <input
