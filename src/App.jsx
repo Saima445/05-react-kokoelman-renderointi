cd5b51ee1c1abf478ef5b855e310732cc1809d42
SaimaS <saima.salonen@gmail.com>
2024-04-02 12:05:35 +0300

    sovellusta laajennettu niin, että note-sisällön voin määrittää tärkeäksi tai ei tärkeäksi.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Note from "./components/Note";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("a new note...");
+  const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
   const hook = () => {
@@ -15,6 +15,7 @@ const App = () => {
     });
   };
   useEffect(hook, []);
+
   //toinen tapa EVENTHANDLERIN KANSSA
   // useEffect(() => {
   //   console.log('effect')
@@ -28,6 +29,7 @@ const App = () => {
   //   promise.then(eventHandler)
   // }, [])
 
+  //kolmas tapa SUORAAN UDEEFFECTILLA
   // useEffect(() => {
   //   console.log(effect);
   //   axios.get("http://localhost:3001/notes").then((response) => {
@@ -37,17 +39,30 @@ const App = () => {
   // }, []);
   // console.log("render", notes.length, "notes");
 
+  const toggleImportanceOf = (id) => {
+    console.log(`importance of ${id} needs to be toggled`);
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    axios.put(url, changedNote).then((response) => {
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    });
+  };
+
   const addNote = (event) => {
     event.preventDefault();
-    console.log("button clicked", event.target);
     const noteObject = {
       content: newNote,
       important: Math.random() > 0.5,
-      id: notes.length + 1,
+      // id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("write another note...");
+    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      console.log(response);
+      setNotes(notes.concat(noteObject));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
@@ -64,37 +79,36 @@ const App = () => {
   return (
     <>
       {/* tapa 1 kovakoodilla */}
-      <h1>Notes (kovakoodi)</h1>
+      {/* <h1>Notes (kovakoodi)</h1>
       <ul>
         {notes.length > 0 && <li>{notes[0].content}</li>}
         {notes.length > 1 && <li>{notes[1].content}</li>}
         {notes.length > 2 && <li>{notes[2].content}</li>}
       </ul>
-      <hr></hr>
+      <hr></hr> */}
       {/* tapa 2 käyttäen map-metodia */}
-      <h1>Notes (map)</h1>
+      {/* <h1>Notes (map)</h1>
       <ul>
         {notes.length > 0 &&
           notes.map((note) => <li key={note.id}>{note.content}</li>)}
       </ul>
-      <hr></hr>
+      <hr></hr> */}
       {/* tapa 3 antipattern, ei ole suositeltava tapa! */}
-      <h1>Notes (taulukon indeksit avaimina)</h1>
+      {/* <h1>Notes (taulukon indeksit avaimina)</h1>
       <ul>
         {notes.length > 0 &&
           notes.map((note, i) => <li key={i}>{note.content}</li>)}
       </ul>
-      <hr></hr>
+      <hr></hr> */}
       {/* tapa 4 tehdään Notelle oma komponentti */}
-      <h1>Notes (omalla komponentilla)</h1>
+      {/* <h1>Notes (omalla komponentilla)</h1>
       <ul>
         {notes.length > 0 &&
           notes.map((note) => <Note key={note.id} note={note} />)}
       </ul>
-      <hr></hr>
-      <hr></hr>
-      {/* tapa 5 tallettaa muuttujaan notesToShow ehdoilla */}
-      <h1>Notes (muuttujalla notesToShow/important:all)</h1>
+      <hr></hr> */}
+      {/* tapa 5 tallettaa muuttujaan notesToShow ehdoilla (muuttujalla notesToShow/important:all) */}
+      <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -102,10 +116,21 @@ const App = () => {
       </div>
       <ul>
         {notes.length > 0 &&
-          notesToShow.map((note) => <Note key={note.id} note={note} />)}
+          notesToShow.map((note) => (
+            <Note
+              key={note.id}
+              note={note}
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
+          ))}
       </ul>
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
+        <input
+          id="noteInput"
+          name="note"
+          value={newNote}
+          onChange={handleNoteChange}
+        />
         <button type="submit">save</button>
       </form>
     </>
